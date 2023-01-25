5c8bf849c468915013419eb213f3c2d26699ed5f
Saiteja <saitejamacha123@gmail.com>
2023-01-25 18:37:34 +0000

    popular

--- src/components/LanguageFilterItem/index.js
+++ src/components/LanguageFilterItem/index.js
@@ -1,1 +1,24 @@
-// Write your code here
+import './index.css'
+
+// Sai Tej's Code
+
+const LanguageFilterItem = props => {
+  const {languageDetails, onClickLanguare, isActive} = props
+  const {language, id} = languageDetails
+
+  const btnStyle = isActive ? 'language-button active' : 'language-button'
+
+  const updatingLanguage = () => {
+    onClickLanguare(id)
+  }
+
+  return (
+    <li>
+      <button className={btnStyle} onClick={updatingLanguage}>
+        {language}
+      </button>
+    </li>
+  )
+}
+
+export default LanguageFilterItem
